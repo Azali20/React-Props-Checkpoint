41d945aa6fe6b114933e0899e159a2491ae1d512
Azali.20 <clintonfaraday@gmail.com>
2023-07-04 08:54:58 +0100

    React Props Checkpoint

---
+++ src/players.js
@@ -0,0 +1,80 @@
+
+    const players = [
+        {
+        name: "Cristiano Ronaldo",
+        team: "Manchester United",
+        nationality: "Portugal",
+        jerseyNumber: 7,
+        age: 36,
+        imageUrl:"images/Ronaldo.webp",
+        // public\images\Osimhen.webp
+        },
+        
+        {
+        name: "Lionel Messi",
+        team: "Paris Saint-Germain",
+        nationality: "Argentina",
+        jerseyNumber: 30,
+        age: 34,
+        imageUrl:"images/Messi.jpeg",
+        },
+
+        {
+        name: "Marcus Rashford",
+        team: "Manchester United",
+        nationality: "England",
+        jerseyNumber: 10,
+        age: 25,
+        imageUrl: "images/Rashford.webp",
+        },
+
+        {
+        name: "Vinicius Junior",
+        team: "Real Madrid",
+        nationality: "Brazil",
+        jerseyNumber: 7,
+        age: 22,
+        imageUrl: "images/Vinicius.jpeg"
+        },
+
+        {
+        name: "Kylian Mbape",
+        team: "PSG",
+        nationality: "France",
+        jerseyNumber: 7,
+        age: 24,
+        imageUrl: "images/Mbape.jpg"
+        },
+
+        {
+        name: "Ngolo Kante",
+        team: "Chelsea",
+        nationality: "France",
+        jerseyNumber: 7,
+        age: 32,
+        imageUrl: "images/Kante.jpeg"
+        },
+
+        {
+        name: "Victor Osihmen",
+        team: "Napoli",
+        nationality: "Nigeria",
+        jerseyNumber: 9,
+        age: 24,
+        imageUrl: "images/Osimhen.webp"
+        },
+
+        {
+        name: "Robert Lewandoski",
+        team: "Barcelona",
+        nationality: "Poland",
+        jerseyNumber: 9,
+        age: 34,
+        imageUrl: "images/Lewandoski.jpg"
+        },
+    ];
+    
+
+    
+    export default players;
+    
